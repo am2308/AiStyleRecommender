73a4e60ed2128a58903028163f6f29bea29dfd12
am2308 <b110020510@gmail.com>
2025-06-29 17:43:09 +0100

    Fix Blank Screen After Deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -69,6 +69,7 @@ export default defineConfig(({ mode }) => {
       open: true,
       cors: true,
     },
+    base: '/',
     define: {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
     }
